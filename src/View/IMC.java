3bf2f461114dc7de7c4dbf3f5fb10a6e0e4d0644
Raissa <raissakestring23@gmail.com>
2025-04-03 21:22:22 -0300

    try to implement a button

--- src/View/IMC.java
+++ src/View/IMC.java
@@ -2,11 +2,14 @@ package View;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 
 public class IMC extends JFrame {
     private JPanel pnlPrincipal;
     private JLabel lblTextoPainel;
     private JLabel lblMiniTexto;
+    private JButton btnPeso;
+    private JButton btnAltura;
 
     public IMC() {
         setTitle("Índice de massa corporal");
@@ -21,19 +24,34 @@ public class IMC extends JFrame {
     private void Components() {
 
         pnlPrincipal = new JPanel();
+        pnlPrincipal.setLayout(null);
+
         pnlPrincipal.setBounds(10, 115, 500, 100);
         pnlPrincipal.setBackground(new Color( 100, 172, 193));
         getContentPane().add(pnlPrincipal);
 
         lblTextoPainel = new JLabel("Indice de massa corporal(IMC)");
+        lblTextoPainel.setBounds(10, 10, 200, 25);
         lblTextoPainel.setForeground(Color.white);
         pnlPrincipal.add(lblTextoPainel);
-        getContentPane().add(pnlPrincipal);
 
         lblMiniTexto = new JLabel("Uma medida da gordura corporal em adultos");
-        lblMiniTexto.setBounds(10, 200, 1, 100);
         lblMiniTexto.setForeground(Color.white);
+        lblMiniTexto.setBounds(10, 23, 500, 25);
         pnlPrincipal.add(lblMiniTexto);
-        getContentPane().add(lblMiniTexto);
+        getContentPane().add(pnlPrincipal);
+
+        btnPeso = new JButton(new AbstractAction("Peso") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        btnPeso.setBounds(65, 150, 250, 25);
+        getContentPane().add(btnPeso);
+
+        lblTextoPainel.setBounds(10, 10, 200, 25);
+        btnPeso.setLayout(null);
     }
 }
